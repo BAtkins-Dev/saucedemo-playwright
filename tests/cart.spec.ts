759e35bf4d3eb40f3ddfc6da165f26fa6194c489
BAtkins <nwman71@gmail.com>
2026-09-19 18:10:37 -0700

    added remove from cart test

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -20,3 +20,18 @@ test("click Add to cart for an item and see that the item is added to the shoppi
     "Remove",
   );
 });
+
+test("remove an item from the cart - cart is empty", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.getByRole("textbox", { name: "Username" }).fill("standard_user");
+  await page.getByRole("textbox", { name: "Password" }).fill("secret_sauce");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await page.getByTestId("add-to-cart-sauce-labs-bolt-t-shirt").click();
+
+  await page.getByTestId("remove-sauce-labs-bolt-t-shirt").click();
+  await expect(
+    page.getByTestId("add-to-cart-sauce-labs-bolt-t-shirt"),
+  ).toHaveText("Add to cart");
+  await expect(page.getByTestId("shopping-cart-link")).toHaveText("");
+});
